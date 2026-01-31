4a1a081849846f6f83b1688fee6e612afabdf602
masrafi143 <masrafiofficial143@gmail.com>
2026-02-01 01:54:39 +0600

    return from methods

---
+++ ReturnFromMethods.java
@@ -0,0 +1,10 @@
+public class ReturnFromMethods {
+    public int add(int a, int b){
+        return a+b;
+    }
+    
+    static void main(String[] args) {
+        ReturnFromMethods c1 = new ReturnFromMethods();
+        System.out.println(c1.add(5,5));
+    }
+}
